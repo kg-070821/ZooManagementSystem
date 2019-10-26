41e744694822189890cc331d5fb9ed3af0f463bc
kg-070821 <57024589+kg-070821@users.noreply.github.com>
2019-10-26 08:05:55 +0530

    Update Cashier.java

--- FRONT END/src/Cashier.java
+++ FRONT END/src/Cashier.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 //kg
-/*
+/*av
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
